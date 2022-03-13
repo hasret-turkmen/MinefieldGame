2445d158aef07c972a76a836b563640adbbb7461
Hasret Türkmen <96438365+hasret-turkmen@users.noreply.github.com>
2022-03-13 23:06:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 public class Main {
+	
 public static void main(String[] args) {
+	
 Scanner bili = new Scanner( System.in );
+	
 Minefield player = new Minefield();
+	
 System.out.println( "Which direction to move?" );
 String direc= bili.next();
 
